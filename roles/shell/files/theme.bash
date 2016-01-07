62760e4da142ac70d75b2584ef5496873e3c7dd6
Oscar Söderlund <poscar@spotify.com>
2016-01-07 23:49:16 +0000

    [shell] Add commits ahead counter to git prompt

--- roles/shell/files/theme.bash
+++ roles/shell/files/theme.bash
@@ -1,11 +1,21 @@
 #!/usr/bin/env bash
 
-git_dirty_state() {
-  (git status -bs --porcelain 2> /dev/null | grep -vq '^#') && echo '*'
-}
-
 color() {
   echo '\[\e[0;'"$1"'m\]'
+}
+
+git_dirty_state() {
+  dirty_files=$(git status -bs --porcelain 2>/dev/null | grep -v '^#')
+  [[ -n $dirty_files ]] && echo "*"
+}
+
+git_ahead_state() {
+  num_commits_ahead=$(git status -bs --porcelain 2>/dev/null \
+    | head -1 | grep ahead | sed 's/.*ahead \([1..9]\)\+.*/\1/')
+  [[ -n $num_commits_ahead ]] && echo -n "+$num_commits_ahead"
+  num_commits_behind=$(git status -bs --porcelain 2>/dev/null \
+    | head -1 | grep behind | sed 's/.*behind \([1..9]\)\+.*/\1/')
+  [[ -n $num_commits_behind ]] && echo -n "-$num_commits_behind"
 }
 
 git_branch() {
@@ -21,7 +31,7 @@ git_branch() {
 git_prompt() {
   local git_branch=$(which git &> /dev/null && git_branch 2>/dev/null)
   if [[ -n $git_branch ]]; then
-    echo -e "$(color 92)[$(color 96)${git_branch}$(color 91)$(git_dirty_state)$(color 92)]"
+    echo -e "$(color 92)[$(color 96)${git_branch}$(git_ahead_state)$(color 91)$(git_dirty_state)$(color 92)]"
   fi
 }
 
