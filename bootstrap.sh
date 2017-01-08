006238f03e549a689004a43100568511e4d279b5
Oscar Söderlund <poscar@spotify.com>
2017-01-08 16:28:26 +0000

    Use cowsay

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,58 +2,51 @@
 
 set -eu
 
-msg() {
-  echo
-  echo '****************************************'
-  echo "${1} ..."
-  echo '****************************************'
-}
+pacman -Sy
+pacman -S cowsay moreutils
 
-msg 'Collecting variables'
+cowsay 'Collecting variables'
 read -rp 'Hostname: ' hostname
 read -rp 'Username: ' username
 read -rp 'Full name: ' full_name
 read -rp 'Email: ' email
 
-msg 'Creating single bootable /dev/sda1 partition'
+cowsay 'Creating single bootable /dev/sda1 partition'
 sfdisk /dev/sda <<< '2048,,L,*'
 mkfs.ext4 /dev/sda1
 
-msg 'Mounting /dev/sda1 to /mnt'
+cowsay 'Mounting /dev/sda1 to /mnt'
 mount /dev/sda1 /mnt
 
-msg 'Writing /etc/fstab'
+cowsay 'Writing /etc/fstab'
 genfstab -U /mnt >> /mnt/etc/fstab
 
-msg 'Installing moreutils'
-pacman --noconfirm -Sy moreutils
-
-msg 'Configuring Swedish Pacman mirrors'
+cowsay 'Configuring Swedish Pacman mirrors'
 cat \
   <(grep --no-group-separator -A1 Sweden /etc/pacman.d/mirrorlist) \
   <(grep -v --no-group-separator -A1 Sweden /etc/pacman.d/mirrorlist) \
   | sponge /etc/pacman.d/mirrorlist
 
-msg 'Installing Arch Linux to /mnt'
+cowsay 'Installing Arch Linux to /mnt'
 pacstrap /mnt base base-devel
 
-msg 'Setting keyboard layout to dvorak'
+cowsay 'Setting keyboard layout to dvorak'
 echo 'KEYMAP=dvorak' >> /mnt/etc/vconsole.conf
 
-msg 'Setting console font'
+cowsay 'Setting console font'
 echo 'FONT=Lat2-Terminus16' >> /mnt/etc/vconsole.conf
 
-msg 'Setting time zone'
+cowsay 'Setting time zone'
 arch-chroot /mnt \
   ln -s /usr/share/zoneinfo/Europe/Stockholm /etc/localtime
 
-msg 'Configuring locale'
+cowsay 'Configuring locale'
 sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /mnt/etc/locale.gen
 echo 'LANG=en_US.UTF-8' >> /mnt/etc/locale.conf
 arch-chroot /mnt \
   locale-gen
 
-msg 'Installing grub'
+cowsay 'Installing grub'
 arch-chroot /mnt \
   pacman --noconfirm -S grub
 arch-chroot /mnt \
@@ -61,30 +54,30 @@ arch-chroot /mnt \
 arch-chroot /mnt \
   grub-mkconfig -o /boot/grub/grub.cfg
 
-msg 'Generating ramdisk'
+cowsay 'Generating ramdisk'
 arch-chroot /mnt \
   mkinitcpio -p linux
 
-msg 'Enabling dhcpcd service'
+cowsay 'Enabling dhcpcd service'
 arch-chroot /mnt \
   systemctl enable dhcpcd.service
 
-msg 'Creating user'
+cowsay 'Creating user'
 arch-chroot /mnt \
   useradd -m -g users -s /bin/bash "$username"
 echo "${username} ALL=(ALL) NOPASSWD: ALL" >> /mnt/etc/sudoers
 
-msg 'Installing packages required for bootstrapping'
+cowsay 'Installing packages required for bootstrapping'
 arch-chroot /mnt \
   pacman --noconfirm -S grub git ansible
 
-msg 'Cloning pbox'
+cowsay 'Cloning pbox'
 arch-chroot /mnt \
   sudo -u poscar \
     git clone https://github.com/odsod/pbox /home/"$username"/pbox
 sed -i 's#https://github.com/#git@github.com:' /mnt/home/"$username"/pbox/.git/config
 
-msg 'Writing pbox variables'
+cowsay 'Writing pbox variables'
 cat > /mnt/home/"$username"/pbox/vars.json <<EOF
 {
   "name": "${full_name}",
@@ -95,16 +88,16 @@ EOF
 arch-chroot /mnt \
   chown "$username":users /home/"$username"/pbox/vars.json
 
-msg 'Bootstrapping pbox'
+cowsay 'Bootstrapping pbox'
 arch-chroot /mnt \
   sudo -u poscar \
     /home/"$username"/pbox/files/scripts/pbox
 
-msg 'Setting root password'
+cowsay 'Setting root password'
 arch-chroot /mnt \
   passwd
-msg 'Setting user password'
+cowsay 'Setting user password'
 arch-chroot /mnt \
   passwd "$username"
 
-msg 'Done!'
+cowsay 'All done!'
