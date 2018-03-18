7fc62426b71ef46c4825043f2b60ef55a1576dbe
Oscar David Söderlund <odsod@users.noreply.github.com>
2018-03-18 17:47:30 +0100

    mdl: Add markdownlint style

---
+++ files/dotfiles/mdl.rb
@@ -0,0 +1,6 @@
+all
+
+exclude_rule 'fenced-code-language' # Fenced code blocks should have a language specified
+exclude_rule 'first-line-h1' # First line in file should be a top level header
+
+rule 'line-length', code_blocks: false, tables: false
